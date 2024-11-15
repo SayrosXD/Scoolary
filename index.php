f90fa1be75b406879add3b6c924d2474826312fb
SayrosXD <luishebertosuarezflores@gmail.com>
2024-11-15 14:23:06 -0600

    Actualizar index.php

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 // Configuración de la base de datos
-$host = '192.168.1.3';
+$host = 'scholary-luishebertosuarezflores-2522.d.aivencloud.com';
 $dbname = 'Count';
-$username = 'root';
-$password = 'root';
+$username = 'avnadmin';
+$password = 'AVNS_TpA1uNQyhiJ6IKizI6P';
+$port = 20421; // Especifica el puerto aquí
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo json_encode(['success' => false, 'error' => 'Error de conexión: ' . $e->getMessage()]);
+    die();
+}
     
     // Obtener los likes del usuario actual
     $userIp = $_SERVER['REMOTE_ADDR'];
